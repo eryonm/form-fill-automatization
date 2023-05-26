8b6de02a9b461ed5df1ca98be8a240b1913c66b4
eryonm <53997175+eryonm@users.noreply.github.com>
2023-05-26 11:35:16 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
